49af20b9a826a6e3dd5ec0ad5333f38f537f68f8
thiagodev-designer <ferreira.clarodf@gmail.com>
2025-04-04 18:28:14 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
 //desenvolvimento
-export const sequelize = new Sequelize('menssagens','root','',{
+/*export const sequelize = new Sequelize('menssagens','root','',{
   host: 'localhost',
 dialect: 'mysql'
-})
+})*/
 //producao
-/*export const sequelize = new Sequelize('sql10770076','sql10770076','VB4MKz2KJw',{
+export const sequelize = new Sequelize('sql10770076','sql10770076','VB4MKz2KJw',{
     host: 'sql10.freesqldatabase.com',
   dialect: 'mysql'
-})*/
+})
 //export const sequelize = new Sequelize('postgresql://root:gsEjUxeOi6ysmdnGRJJvR7njmxJrzGtL@dpg-cv90qdogph6c73c444b0-a/menssagens')
 sequelize.authenticate()
 
